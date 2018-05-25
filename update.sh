8fcd77455005131b87613c82c7f95b4df6558ec6
Simon Oulevay <simon.oulevay@gmail.com>
2018-05-25 17:19:20 +0200

    Mysql test

--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
 
+dockerfile_file=Dockerfile
+dockerfile_header_file=templates/Dockerfile.header
+dockerfile_footer_file=templates/Dockerfile.footer
 template_file=Dockerfile.template
 variants_dir=variants
-versions_dir=versions
 
-cat Dockerfile.header Dockerfile.footer > Dockerfile
+cat "$dockerfile_header_file" "$dockerfile_footer_file" > "$dockerfile_file"
 echo "base -> Dockerfile"
 
 for variant in $(ls variants); do
 
   variant_file="$variants_dir/$variant"
-  version_file="$versions_dir/Dockerfile.$variant"
+  version_file="Dockerfile.$variant"
   extras="$(cat "$variant_file")"
 
-  cat Dockerfile.header "$variant_file" Dockerfile.footer > "$version_file"
+  cat "$dockerfile_header_file" "$variant_file" "$dockerfile_footer_file" > "$version_file"
 
   echo "$variant_file -> $version_file"
 done
