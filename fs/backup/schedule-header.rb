851424d6b1118e8da04136e5b45f1f3cceeb55f1
Simon Oulevay <simon.oulevay@gmail.com>
2020-04-22 14:18:59 +0200

    Remove superfluous environment option from perform_backup

--- fs/backup/schedule-header.rb
+++ fs/backup/schedule-header.rb
@@ -4,8 +4,8 @@ set :job_template, nil
 set :output, standard: '/var/log/backup.log', error: '/var/log/backup.log'
 
 def perform_backup trigger, options = {}
-  environment = options.fetch :environment, {}
-  export_environment = environment.empty? ? '' : "export#{environment.reduce(''){ |memo,(k,v)| memo << " #{k}=#{Shellwords.shellescape v}" }} && "
-  command "#{export_environment} /usr/local/bin/with-contenv.sh /usr/local/bundle/bin/backup perform --no-logfile --config-file /backup/config.rb --data-path #{Shellwords.shellescape ENV.fetch('BACKUP_DATA_PATH', '/var/lib/backups')} --tmp-path #{Shellwords.shellescape ENV.fetch('BACKUP_TMP_PATH', '/tmp/backup')} -t #{trigger}"
+  data_path = ENV.fetch 'BACKUP_DATA_PATH', '/var/lib/backups'
+  tmp_path = ENV.fetch 'BACKUP_TMP_PATH', '/tmp/backup'
+  command "/usr/local/bin/with-contenv.sh /usr/local/bundle/bin/backup perform --no-logfile --config-file /backup/config.rb --data-path #{Shellwords.shellescape data_path} --tmp-path #{Shellwords.shellescape tmp_path} -t #{trigger}"
 end
 
