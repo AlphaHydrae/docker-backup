8ae44989f52375d168bfe992f81405ca68548473
Simon Oulevay <simon.oulevay@gmail.com>
2021-06-23 16:59:14 +0200

    Use debian stretch for old variants

--- update.sh
+++ update.sh
@@ -6,16 +6,18 @@ dockerfile_footer_file=templates/Dockerfile.footer
 template_file=Dockerfile.template
 variants_dir=variants
 
-cat "$dockerfile_header_file" "$dockerfile_footer_file" > "$dockerfile_file"
-echo "base -> Dockerfile"
+for ruby_docker_image_tag in $(ls variants); do
+  for variant in $(ls variants/$ruby_docker_image_tag); do
+    variant_file="$variants_dir/$ruby_docker_image_tag/$variant"
 
-for variant in $(ls variants); do
+    if [[ "$variant" == "base" ]]; then
+      version_file="Dockerfile"
+    else
+      version_file="Dockerfile.$variant"
+    fi
 
-  variant_file="$variants_dir/$variant"
-  version_file="Dockerfile.$variant"
-  extras="$(cat "$variant_file")"
-
-  cat "$dockerfile_header_file" "$variant_file" "$dockerfile_footer_file" > "$version_file"
-
-  echo "$variant_file -> $version_file"
+    printf "FROM ruby:${ruby_docker_image_tag}\n\n" > "$version_file"
+    cat "$dockerfile_header_file" "$variant_file" "$dockerfile_footer_file" >> "$version_file"
+    echo "$variant_file -> $version_file"
+  done
 done
